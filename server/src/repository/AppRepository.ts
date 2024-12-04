d234919e8bda3aec1ff4aea603bf01c407c15c78
LucasKleaL <lucaskleal222@outlook.com>
2024-12-03 21:27:15 -0300

    Adicionado EmployeeExtraHoursDaysOff
    Server:
    - Adicionado a função getEmployeeExtraHoursAndDaysOff no EmployeeRepository, para buscar os registros de ExtraHours e DaysOff relacionados com cada registro de Employee.
    - Adicionado a função setExtraHoursData no EmployeeRepository para realizar os cálculos de quantas horas extras e horas de folga um Employee possui, além de contabilizar a diferença entre as duas para obter quantas horas restantes ele ainda tem disponíveis para folgar.
    - Adicionado as funções timeStringToMinutes e minutesToTimeString no AppRepository para realizar as conversões necessárias para cálculos, de string em formato de horários.
    
    Client:
    - Refatorado o component card, para utilizar um objeto cardInfos, com as informações do que será exibido no card, ao invés de utilizar diversas propriedades para cada informação. Dessa forma é possível que cada card exiba qualquer quantidade de informação, que é setado no componente pai.
    - Adicionado as informações de quantidade de horas extras, quantidade de folgas e horas extras restantes no card do index-employees.

--- server/src/repository/AppRepository.ts
+++ server/src/repository/AppRepository.ts
@@ -16,6 +16,17 @@ class AppRepository {
         return format(date, "dd/MM/yyyy HH:mm:ss");
     }
 
+    timeStringToMinutes(time: string): number {
+        const [hours, minutes] = time.split(":").map(Number);
+        return hours * 60 + minutes;
+    }
+    
+    minutesToTimeString(minutes: number): string {
+        const hours = Math.floor(minutes / 60);
+        const remainingMinutes = minutes % 60;
+        return `${String(hours).padStart(2, '0')}:${String(remainingMinutes).padStart(2, '0')}`;
+    }
+
 }
 
 export default AppRepository;
